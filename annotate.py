f25f7482beb7e4b3980171275f2cbb20df6b736a
chrisfromthepast <63051844+chrisfromthepast@users.noreply.github.com>
2025-05-14 11:36:18 -0400

    Update annotate.py

--- annotate.py
+++ annotate.py
@@ -5,24 +5,31 @@ import os
 RESOURCE_ROOT = os.path.dirname(__file__)
 
 # Fill the writer with the pages you want
-pdf_path = os.path.join(RESOURCE_ROOT, "E1.62.pdf")
+pdf_path = os.path.join(RESOURCE_ROOT, "E1.62.pdf") #consider making this a variable in the configuration options
 reader = PdfReader(pdf_path)
 page = reader.pages[0]
 writer = PdfWriter()
 writer.add_page(page)
 
-# Create the annotation and add it
-annotation = Text(
-    text="Hello World\nThis is the second line!",
-    rect=(50, 550, 200, 650),
+fields = reader.get_fields()
+print(fields)
+# Fill the form fields
+for field in fields:
+    print(field)
+    if field.get("/FT") == "/Btn":
+        # Check if the field is a button
+        print("Button field found:", field.get("/T"))
+    else:
+        # Handle other types of fields (e.g., text fields)
+        print("Text field found:", field.get("/T"))
+
+writer.append(reader)
+
+writer.update_page_form_field_values(
+    writer.pages[0],
+    {"fieldname": "some filled in text"},
+    auto_regenerate=False,
 )
 
-# Set annotation flags to 4 for printable annotations.
-# See "AnnotationFlag" for other options, e.g. hidden etc.
-annotation.flags = 4
-
-writer.add_annotation(page_number=0, annotation=annotation)
-
-# Write the annotated file to disk
-with open("annotated-pdf.pdf", "wb") as fp:
-    writer.write(fp)
+with open("filled-out.pdf", "wb") as output_stream:
+    writer.write(output_stream)
